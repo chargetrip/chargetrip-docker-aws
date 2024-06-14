75a01aaacfd36017f1c32452bfdb27046b1c075f
Cosmin Petrescu <cosmin.petrescu@chargetrip.com>
2024-06-14 15:21:57 +0300

    Upgrade kubectl

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -t chargetrip-docker-aws:latest .
-docker tag chargetrip-docker-aws:latest chargetripio/chargetrip-docker-aws:latest
-docker push chargetripio/chargetrip-docker-aws:latest
+docker tag chargetrip-docker-aws "152595642771.dkr.ecr.eu-central-1.amazonaws.com/chargetrip-docker-aws:latest"
+docker push "152595642771.dkr.ecr.eu-central-1.amazonaws.com/chargetrip-docker-aws:latest"
